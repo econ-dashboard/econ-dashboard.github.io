454991eaef611617bc106c32289f4c0f4c1d1e19
Rachel Oh <racheloh@stanford.edu>
2021-05-13 05:50:21 +0900

    pushing up lastest changes

--- site/static/js/fuse-search.js
+++ site/static/js/fuse-search.js
@@ -1,16 +1,31 @@
-import Fuse from 'fuse.js';
+import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@6.4.6/dist/fuse.esm.js";
 
 const countyNames = [
     {
-        "county": "Santa Clara"
+        county: "Santa Clara",
     },
     {
-        "county": "Marin" 
+        county: "Santa Cruz",
+    },
+    {
+        county: "Marin",
+    },
+    ];
+
+    const options = {
+    includeScore: true,
+    findAllMatches: true,
+    keys: ["county"],
+    };
+
+    const searchInput = document.querySelector("input");
+    // console.log("search input: ", searchInput);
+
+    searchInput.addEventListener("input", search);
+    // search();
+    
+    function search() {
+    const fuse = new Fuse(countyNames, options);
+    const result = fuse.search(searchInput.value).value;
+    document.querySelector(".dropdown").innerHTML = result;
     }
-]
-
-const fuse = new Fuse(counties, {
-    keys: ['county']
-});
-
-const results = fuse.search('Santa')
