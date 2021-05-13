9a02d483a918fe0d3db93297d7c5d7a03cfd994a
Rachel Oh <racheloh@stanford.edu>
2021-05-14 02:16:02 +0900

    troubleshooting with collapsible

--- site/static/js/home.js
+++ site/static/js/home.js
@@ -1,31 +1,26 @@
-// JS for search
+async function loadJSON() {
+    let requestJSON = new Request('county-data/json/json_for_search.json');
+    let fetchResponse = await fetch(requestJSON);
+    let jsonValue = await fetchResponse.json();
+    var resolvedJSONValue = Promise.resolve(jsonValue);
+    console.log(resolvedJSONValue.typeof);
+    // here I get undefined for type and I wasn't sure if that should be the case
+    return resolvedJSONValue;
+}
 
-
-// let requestURL = 'county-data/json/json_for_search.json';
-// let request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-// request.onload = function() {
-//     const countyList = request.response;
-//     getElementsByClassName(".dropdown").innerHTML = countyList;
+// stateButton.addEventListener("click", () => {
 //   }
 
+var object = Object.keys(resolvedJSONValue);
+console.log(object);
+
+stateButton.addEventListener("click", () => {
+  // things to come 
+}; 
 
 
 
 
-//  JS for accordian 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
+
+
+
